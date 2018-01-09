9edcf7ede7ad226708aae9c3acbd104db89f784d
greeneyes8 <31348443+greeneyes8@users.noreply.github.com>
2018-01-09 17:58:35 +0100

    Add files via upload

--- src/ai/montecarloheuristic10/MonteCarloH10.java
+++ src/ai/montecarloheuristic10/MonteCarloH10.java
@@ -70,9 +70,11 @@ public class MonteCarloH10 {
 	 * @throws Exception 
 	 */
 	private Node treePolicy(Node node, Board board) {
-		while(node.getMoveNumber() < this.allMovesNumber) {
+		int numberNode = node.getMoveNumber() ;
+		while(numberNode < this.allMovesNumber) {
 			if(node.getUntriedMoves().size() != 0) {
 				Node newNode =  node.expand(board, this.color);
+				numberNode = node.getMoveNumber() ;
 				return newNode;
 			} else {
 				String color = node.getColor();
@@ -106,7 +108,8 @@ public class MonteCarloH10 {
 		Random generator = new Random();
 		String color = node.getColor();
 		int moveNumber = node.getMoveNumber();
-
+		String w = "w";
+		
 		while(moveNumber < this.allMovesNumber) {
 			List<Tuple<Integer, Integer>> listValidMoves;
 			if(color.equals(this.color)) {
@@ -115,7 +118,7 @@ public class MonteCarloH10 {
 				listValidMoves = board.getListValidMoves();
 			}
 			board.makeMove(listValidMoves.get(generator.nextInt(listValidMoves.size())), color);
-			if(color == ("w")) {
+			if(color.equals(w)) {
 				color = "b";
 			} else {
 				color = "w";
@@ -179,8 +182,9 @@ public class MonteCarloH10 {
 	 */
 	private void back_up(Node node, String delta) {
 		double value;
+		String zero = "0";
 		
-		if(delta == ("0")) {
+		if(delta.equals(zero)) {
 			value = .5;
 		} else if(delta.equals(node.getColor())) {
 			value = 0;
